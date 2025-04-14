7fa34ad2b9448278df0e86cf51fe723877950eb3
MikiZivanovic <miodrag.zivanovic23@gmail.com>
2025-04-14 13:01:58 +0200

    Added Yarp gateway

---
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -0,0 +1,26 @@
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("customPolicy", opt =>
+    {
+        opt.PermitLimit = 7;
+        opt.Window = TimeSpan.FromSeconds(12);
+        
+    });
+});
+
+var app = builder.Build();
+
+app.UseRateLimiter();
+
+app.MapReverseProxy();
+
+
+app.Run();
